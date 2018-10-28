b28142e99029883ccf0defd58ced7bd00031b60f
Petros G. Sideris <pgsideris@gmail.com>
2018-10-28 19:47:19 +0000

    add linked list

---
+++ javascript/linkedList.js
@@ -0,0 +1,9 @@
+// Your task is to create a simple linked list implementation.
+// You can find the theory in Wikipedia: https://en.wikipedia.org/wiki/Linked_list
+// LinkedList should support insertion (simple and after / before node), deletion and search
+// Once you are done with this you should create a double linked list. What's the benefit in that?
+// Other extension goals: Circular Linked List, Circular Double Linked list
+
+class Node {
+
+}
